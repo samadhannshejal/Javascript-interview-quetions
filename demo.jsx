8c0021aa1e5289e09a51a5779056ff19f117ca12
samadhan <samadhannshejal99@gmail.com>
2024-09-10 16:36:01 +0530

    9-10

--- demo.jsx
+++ demo.jsx
@@ -1,11 +1,74 @@
 //! Closures
 
-const parentFun=()=>{
-    let outerVariable="I am outside"
-    const innerFun=()=>{
-        console.log(outerVariable)
-    }
-    return innerFun
-}
-const child=parentFun();
-child()
+// const parentFun=()=>{
+//     let outerVariable="I am outside"
+//     const innerFun=()=>{
+//         console.log(outerVariable)
+//     }
+//     return innerFun
+// }
+// const child=parentFun();
+// child()
+
+//! This keyword
+
+//! Global Object:-
+
+//  function fun(){
+//     console.log(this)
+//  }
+//   fun()  | window.fun()
+
+//! Objects methods : -
+
+// const obj={
+//     name:"samadhan",
+//     func:function(){
+//         console.log(this) // object itself
+//     }
+// }
+// obj.func(); // function called as method of object
+
+
+// const obj2={
+//     name:"samadhan",
+//     func:function(){
+//         console.log(this) // global object
+//     }
+// }
+// const fun1=obj.func(); // function called as method of object
+// fun1()
+
+//! Constructor function
+// function Person(){
+//     console.log(this); // pointing towards to newly created object from instance
+//  }
+
+// const person= new Person()
+// person.name="samadhan"
+
+//! Arrow function
+
+// const fun=()=>{
+//     console.log(this) // global 
+//   } 
+//   fun()
+
+// const obj={
+//     name :"samadhan",
+//     func:()=>{
+//        console.log(this)// global
+//     }
+//   } 
+//   obj.func() 
+
+// const obj={
+//     name :"samadhan",
+//     func:function(){
+//       const innerFun= ()=>{
+//        console.log(this)// obj itself m, its this inherit from surrounding context
+//     }
+//     innerFun()
+//     }
+//   } 
+//   obj.func() 
